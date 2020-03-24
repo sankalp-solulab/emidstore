627a6a063ea4402d6fbbf23b02c50a8eb3f8a898
sheetaljaiswal <sheetal@solulab.co>
2020-03-24 18:35:58 +0530

    ContactUs component and data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { ConnectedRouter } from "connected-react-router";
 import { Provider } from "react-redux";
 import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
-import asyncComponent from "./utils/asyncComponent.jsx";
+import asyncComponent from './utils/asyncComponent.jsx';
 import * as serviceWorker from "./serviceWorker";
-import axios from "./services";
+import axios from './services';
 import configureStore, { history } from "./store";
-export const store = configureStore();
 import "./assets/css/animation/animate.css";
 import "./assets/css/bootstrap/bootstrap.css";
 import "./assets/css/owl-carousel/owl.carousel.css";
@@ -19,24 +18,37 @@ import "./assets/css/fix-scroll.css";
 import "./assets/css/responsive.css";
 import "./assets/css/style.css";
 import "./assets/css/map.css";
+import contactUs from "./components/contactUs/contactUs.jsx";
+import contactUsForrm from "./components/contactUs/contactUsForm/contactUsForrm.jsx";
+export const store = configureStore();
 
-const demoHTML = () => <div>HELLO</div>;
+
+const demoHTML = () => (<div>HELLO</div>);
 const App = () => {
-  return (
-    <>
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <BrowserRouter>
-            <Switch>
-              <Route path="/" component={demoHTML} />
-            </Switch>
-          </BrowserRouter>
-        </ConnectedRouter>
-      </Provider>
-    </>
-  );
-};
+    return (
+        <>
+            <Provider store={store}>
+                <ConnectedRouter history={history}>
+                    <BrowserRouter>
+                        <Switch>
+                           
+                           
+                            <Route path="/" component={contactUs} />
+                            {/* <Route path="/" component={contactUsForrm} /> */}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+                        </Switch>
+                    </BrowserRouter>
+                </ConnectedRouter>
+            </Provider>
+        </>
+    );
+}
+
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
 serviceWorker.register();
 export default App;
