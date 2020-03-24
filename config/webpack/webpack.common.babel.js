a6c8ed2076f8ebdc7d72fe408550838914da50fe
dhrumil shah <hasija@solulab.co>
2020-03-24 12:11:51 +0530

    initial commit

--- config/webpack/webpack.common.babel.js
+++ config/webpack/webpack.common.babel.js
@@ -1,40 +1,38 @@
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-import path from 'path';
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import path from 'path'
 
-import paths from './paths';
-import rules from './rules';
+import paths from './paths'
+import rules from './rules'
 
-const ROOT = paths.outputPath;
+const ROOT = paths.outputPath
 
 function root (args) {
-    args = Array.prototype.slice.call (arguments, 0);
-    return path.join.apply (path, [ROOT].concat (args));
+  args = Array.prototype.slice.call(arguments, 0)
+  return path.join.apply(path, [ROOT].concat(args))
 }
 
 module.exports = {
-    entry: paths.entryPath,
-    module: {
-        rules
-    },
-    resolve: {
-        modules: ['src', 'node_modules'],
-        extensions: ['*', '.js', '.scss', '.css']
-    },
-    plugins: [
-        new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({
-            template: paths.templatePath,
-            favicon: 'src/assets/img/favicon.ico',
-            minify: {
-                collapseInlineTagWhitespace: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: true,
-                minifyURLs: true,
-                removeComments: true,
-                removeAttributeQuotes: true
-            }
-        })
-    ]
-};
+  entry: paths.entryPath,
+  module: {
+  rules},
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.scss', '.css']
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: paths.templatePath,
+      minify: {
+        collapseInlineTagWhitespace: true,
+        collapseWhitespace: true,
+        preserveLineBreaks: true,
+        minifyURLs: true,
+        removeComments: true,
+        removeAttributeQuotes: true
+      }
+    })
+  ]
+}
